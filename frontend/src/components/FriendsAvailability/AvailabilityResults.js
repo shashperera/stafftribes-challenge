19a6698f530ad254e7a791911fa1dea5e10f9166
shashperera <sanduni.p@sliit.lk>
2023-11-27 01:31:16 +0200

    getFriends

--- frontend/src/components/FriendsAvailability/AvailabilityResults.js
+++ frontend/src/components/FriendsAvailability/AvailabilityResults.js
@@ -8,17 +8,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Button } from '@mui/material';
-import Friends from './Friends';
 
-// function createData(id, name, availability, action) {
-//   return { id, name, availability, action };
-// }
-
-// const rows = [
-//   createData(1, 'Shashi', 1, 'View'),
-//   createData(2, 'Rakitha', 237, 'View'),
-//   createData(3, 'Madhara', 262, 'View'),
-// ];
 
 function AvailabilityResults({ id, name, availability, action }) {
   return (
@@ -34,13 +24,13 @@ function AvailabilityResults({ id, name, availability, action }) {
           </TableHead>
           <TableBody>
             <TableRow
-              key={id}
+              // key={id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
                 {name}
               </TableCell>
-              <TableCell align="right">Weeks {availability}</TableCell>
+              <TableCell align="right">Weeks {availability.join(', ')}</TableCell>
               <TableCell align="right"><Button>View</Button></TableCell>
             </TableRow>
 
--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,33 +1,38 @@
-import React, { useContext, useState } from "react"
-import axios from 'axios'
+import React, { useContext, useState } from "react";
+import axios from 'axios';
 
-//axios to connect to server
+// Axios to connect to the server
 const BASE_URL = "http://localhost:5000/api/";
 
-
+// Creating a context for global state
 const GlobalContext = React.createContext();
 
+// Global Provider component
 export const GlobalProvider = ({ children }) => {
-    const [friends, setFriends] = useState([]);
+  // State to store friends data
+  const [friends, setFriends] = useState([]);
 
-    const getFriends = async () => {
-        try {
-            const response = await axios.get(`${BASE_URL}`);
-            setFriends(response.data);
-            return response.data;
-        } catch (error) {
-            console.error("Error fetching friends:", error);
-            return [];
-        }
-    };
+  // Function to fetch friends data from the server
+  const getFriends = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}`);
+      setFriends(response.data);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching friends:", error);
+      return [];
+    }
+  };
 
-    return (
-        <GlobalContext.Provider value={{ friends, getFriends }}>
-            {children}
-        </GlobalContext.Provider>
-    );
+  // Providing the state and functions through the context
+  return (
+    <GlobalContext.Provider value={{ friends, getFriends }}>
+      {children}
+    </GlobalContext.Provider>
+  );
 };
 
+// Custom hook to access the global context
 export const useGlobalContext = () => {
-    return useContext(GlobalContext)
-}
+  return useContext(GlobalContext);
+};
