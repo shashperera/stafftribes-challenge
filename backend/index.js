446edea50fbb3cf1fa961531d8c25ec67fdd3dac
shashperera <sanduni.p@sliit.lk>
2023-11-29 04:07:16 +0200

    V1 deployment

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({
+  origin: 'https://vercel.com/'
+}));
 
 // Use the router for a specific path
 readdirSync('./routes').map((route) => app.use('/api/', require('./routes/' + route)));
